fc79e93d5fe68a8b2f625b0ede7f5b7bee3e160e
johnzwiss <johnzwiss@gmail.com>
2022-04-21 14:57:45 -0400

    student model added

--- capstone_project/main_app/models.py
+++ capstone_project/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    lessons_completed = ArrayField(base_field=models.CharField(max_length=200, null=True), default=list, blank=True)
+
+
+
+
