3d77259b84a37f55b6d2794d71090e2ebb1c7bef
johnzwiss <johnzwiss@gmail.com>
2022-05-05 10:33:27 -0400

    debug on

--- capstone_project/capstone_project/settings.py
+++ capstone_project/capstone_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r08+vm0y@*!*h!e(56+a_4vc#bsoz&g2o&9l=_#dyi$^34*fri'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*',]
 
